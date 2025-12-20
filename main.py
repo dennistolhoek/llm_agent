6ae96d2ba1cfc3ff5f27c93a0ddacec1d17e5259
dennis_tolhoek <dennistolhoek@gmail.com>
2025-12-20 14:53:16 +0100

    uploading completed project of the boot.dev llm_agent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -7,6 +8,7 @@ from google.genai import types
 
 from call_function import available_functions, call_function 
 from prompts import system_prompt  
+from config import MAX_ITERS
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -18,13 +20,27 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
+   
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     if args.verbose:
         print(f"User prompt: {args.user_prompt}\n")
-    generate_content(client, messages, args.verbose)
-    #print(args.verbose)
-
+    
+    iterations = 0
+    while True:
+        iterations += 1
+        if iterations > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+        try:
+            final_response = generate_content(client, messages, args.verbose)
+            if final_response:
+                print("Final Response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+    
 def generate_content(client, messages, verbose):
     
     response = client.models.generate_content(
@@ -40,10 +56,12 @@ def generate_content(client, messages, verbose):
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call = candidate.content
+            messages.append(function_call)
     if not response.function_calls:
-        print("Response:")
-        print(response.text)
-        return
+        return response.text
 
     function_responses = []    
     for function_call in response.function_calls:
@@ -57,7 +75,13 @@ def generate_content(client, messages, verbose):
         if verbose:
             print(f"-> {result.parts[0].function_response.response}")
         function_responses.append(result.parts[0])
-
+    
+    messages.append(
+        types.Content(
+            role="user",
+            parts=function_responses
+        )
+    )
 
 if __name__ == "__main__":
     main()
